28842a2dabc3327ba4abf9eea129ddf09f367eb6
Jory Roberts <robertsjory13@gmail.com>
2023-03-26 23:19:52 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((tutorial) => {
+    let modifyWords = tutorial.split(" "); // split each tutorial into array
+    let capitalizeWord = modifyWords.map(
+      (word) => word.charAt(0).toUpperCase() + word.slice(1)
+    ); // map each word of the array and capitalize first letter
+    let newString = capitalizeWord.join(" "); //combine capitalized word back into string
+    return newString;
+  });
+};
